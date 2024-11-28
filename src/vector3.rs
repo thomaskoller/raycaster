29b87d3fe513bfe972f1f4c42d449125aa8d92b6
thomaskoller <thomaskoller90@icloud.com>
2024-11-28 21:17:49 +0100

    feat: Add implementation of Vector3

---
+++ src/vector3.rs
@@ -0,0 +1,58 @@
+#[derive(PartialEq, Copy, Clone, Debug)]
+pub struct Vector3 {
+    x: f64,
+    y: f64,
+    z: f64,
+}
+
+impl Vector3 {
+    pub fn new(x: f64, y: f64, z: f64) -> Self {
+        Self { x, y, z }
+    }
+
+    pub fn dot(&self, other: &Self) -> f64 {
+        self.x * other.x + self.y * other.y + self.z * other.z
+    }
+
+    pub fn length(&self) -> f64 {
+        self.dot(self).sqrt()
+    }
+
+    pub fn normalize(&self) -> Self {
+        let len = self.length();
+        Self::new(self.x / len, self.y / len, self.z / len)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::vector3::Vector3;
+
+    #[test]
+    fn can_create_vector() {
+        let vector3 = Vector3::new(1.0, 2.0, 3.0);
+        assert_eq!(vector3.x, 1.0);
+        assert_eq!(vector3.y, 2.0);
+        assert_eq!(vector3.z, 3.0);
+    }
+
+    #[test]
+    fn can_calculate_dot_product() {
+        let first = Vector3::new(1.0, 2.0, 3.0);
+        let second = Vector3::new(4.0, 5.0, 6.0);
+        assert_eq!(first.dot(&second), 32f64);
+    }
+    #[test]
+    fn can_calculate_length() {
+        let vector3 = Vector3::new(1.0, 0.0, 0.0);
+        assert_eq!(vector3.length(), 1f64);
+    }
+    #[test]
+    fn can_normalize() {
+        let vector3 = Vector3::new(10.0, 0.0, 0.0);
+        let normalized = vector3.normalize();
+        assert_eq!(normalized.x, 1f64);
+        assert_eq!(normalized.y, 0f64);
+        assert_eq!(normalized.z, 0f64);
+    }
+}
